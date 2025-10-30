82982a8b4cfe5a517310d696be6d27442d843632
Jacob O'Sullivan <Jacob.OSullivan@ForestResearch.gov.uk>
2025-10-30 10:21:24 +0000

    CO2 and CH4 emissions output by CO2_loss_from_drained_peat instead of just total in CO2 eq. units. Required by DOC/POC loss module.

--- R/5d_CO2_loss_from_drained_peat.R
+++ R/5d_CO2_loss_from_drained_peat.R
@@ -24,7 +24,7 @@ CO2_loss_drained <- function(core.dat, AV_indirect, R_tot) {
 
   L_indirect <- list(L_drained = L_drainage$L_drained,
                      L_undrained = L_drainage$L_undrained,
-                     L_indirect = L_drainage$L_drained - L_drainage$L_undrained)
+                     L_indirect = L_drainage$L_drained$Tot - L_drainage$L_undrained$Tot)
 
   return(L_indirect)
 }
@@ -66,14 +66,25 @@ CO2_loss_drained0 <- function(pC_dry_peat,
   # If restored emissions from drained and counterfactual (undrained) calculated using ECOSSE factors
   A_t <- (A_indirect / 10000) * (t_wf + t_restore)  # convert area to ha
   pD_f <- D_f / 365
-  L_drained_rest <- A_t * (R_tot$R_CO2_drained + R_tot$R_CH4_drained) # D_f = 0
-  L_undrained_rest <- A_t * (((R_tot$R_CO2_undrained * pD_f) + (R_tot$R_CO2_drained * (1 - pD_f))) +
-                             ((R_tot$R_CH4_undrained * pD_f) + (R_tot$R_CH4_drained * (1 - pD_f))))
-  p_undrained <- L_undrained_rest / L_drained_rest # proportion used in case not restored
+
+  CO2_drained_rest <- A_t * (R_tot$R_CO2_drained)
+  CH4_drained_rest <- A_t * (R_tot$R_CH4_drained)
+  L_drained_rest <- list(Tot = CO2_drained_rest + CH4_drained_rest,
+                         CO2 = CO2_drained_rest,
+                         CH4 = CH4_drained_rest) # D_f = 0
+
+  CO2_undrained_rest <- A_t * ((R_tot$R_CO2_undrained * pD_f) + (R_tot$R_CO2_drained * (1 - pD_f)))
+  CH4_undrained_rest <- A_t * ((R_tot$R_CH4_undrained * pD_f) + (R_tot$R_CH4_drained * (1 - pD_f)))
+  L_undrained_rest <- list(Tot = CO2_undrained_rest + CH4_undrained_rest,
+                           CO2 = CO2_undrained_rest,
+                           CH4 = CH4_undrained_rest)
+
+
+  p_undrained <- L_undrained_rest$Tot / L_drained_rest$Tot # proportion used in case not restored
 
   # If not restored, all carbon assumed lost, counterfactual given by ratio computed above
-  L_drained_no_rest <- (CO2_C / 100) * pC_dry_peat * BD_dry_soil * V_indirect
-  L_undrained_no_rest <- L_drained_no_rest * p_undrained
+  L_drained_no_rest <- list(Tot = (CO2_C / 100) * pC_dry_peat * BD_dry_soil * V_indirect)
+  L_undrained_no_rest <- list(Tot = L_drained_no_rest$Tot * p_undrained)
 
   if (restore_hab_in[1] == 2 & restore_hydr_in[1] == 2) { # habitat AND hydrology restored
     return(list(L_drained = L_drained_rest,
@@ -82,5 +93,4 @@ CO2_loss_drained0 <- function(pC_dry_peat,
     return(list(L_drained = L_drained_no_rest,
                 L_undrained = L_undrained_no_rest))
   }
-
 }
