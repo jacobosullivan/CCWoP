4b41d263144e259e7cd5f99cc75213b1e845f73e
Jacob O'Sullivan <Jacob.OSullivan@ForestResearch.gov.uk>
2025-10-30 11:22:20 +0000

    DOC and POC loss added

--- R/6_CO2_loss_by_DOC_and_POC_loss.R
+++ R/6_CO2_loss_by_DOC_and_POC_loss.R
@@ -1,10 +1,56 @@
 ## 6. CO2 loss by DOC & POC loss
 
-CO2_loss_DPOC0 <- function() {
-  # For total gaseous carbon losses it seems like CO2 and CH4 need to be handled seperately due to
-  # differences in mol. weight
-  # I have summed theses elsewhere
-  if (restore_hab_in[1] == 2 & restore_hydr_in[1] == 2) {
+#' CO2_loss_DOC_POC
+#' @param core.dat UI data
+#' @return L_DOC
+#' @export
+CO2_loss_DOC_POC <- function(core.dat,
+                             L_indirect,
+                             L_improvement,
+                             pC_DOC = c(Exp = 26, Min = 7, Max = 40),
+                             pDOC_CO2 = 100,
+                             pC_POC = c(Exp = 8, Min = 4, Max = 10),
+                             pPOC_CO2 = 100,
+                             CO2_C = 3.667,
+                             CH4_CO2 = 30.67,
+                             pC_CH4 = 0.75) {
 
+  # THIS FUNCTION...
+
+  if (core.dat$Site.restoration$restore_hab_in[1] == 2 & core.dat$Site.restoration$restore_hydr_in[1] == 2) {
+
+    L_CO2_drain_rest <- L_indirect$L_drained$CO2 - L_indirect$L_undrained$CO2
+    L_CO2_drain_rest[L_CO2_drain_rest<0] <- 0
+
+    L_CH4_drain_rest <- L_indirect$L_drained$CH4 - L_indirect$L_undrained$CH4
+    L_CH4_drain_rest[L_CH4_drain_rest<0] <- 0
+
+    L_CO2_improved <- list_op(l1 = L_improvement$E_CO2_aft, l2 = L_improvement$E_CO2_bfr, func = "-")
+    L_CO2_improved <- lapply(L_CO2_improved, FUN = function(x) {
+                        x[x<0] <- 0
+                        return(x)
+                      })
+    L_CH4_improved <- list_op(l1 = L_improvement$E_CH4_aft, l2 = L_improvement$E_CH4_bfr, func = "-")
+    L_CH4_improved <- lapply(L_CH4_improved, FUN = function(x) {
+      x[x<0] <- 0
+      return(x)
+    })
+
+    L_CO2_improved$Restored <- L_CO2_drain_rest
+    L_CH4_improved$Restored <- L_CH4_drain_rest
+    L_C_Tot <- colSums(bind_rows(L_CO2_improved) / CO2_C) + colSums(bind_rows(L_CH4_improved) * pC_CH4 / CH4_CO2) #
+    L_C_DOC <- L_C_Tot * pC_DOC / 100
+    L_C_DOC[L_C_DOC<0] <- 0
+    L_C_POC <- L_C_Tot * pC_POC / 100
+    L_C_POC[L_C_POC<0] <- 0
+
+    L_DOC <- L_C_DOC * (pDOC_CO2 / 100) * CO2_C
+    L_POC <- L_C_POC * (pPOC_CO2 / 100) * CO2_C
+
+    L_DPOC <- L_DOC + L_POC
+  } else { # if not restoring, DOC and POC are already accounted for (all C lost)
+    L_DPOC <- c(Exp = 0, Min = 0, Max = 0)
   }
+
+  return(L_DPOC)
 }
